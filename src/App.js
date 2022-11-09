d3b5b50eaf5a1cee94120851c8e0e31feb44288c
Bas Bruin <487790@student.fontys.nl>
2022-11-09 12:50:14 +0100

    Add user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MenuAppbar from './components/Appbar';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
       <MenuAppbar/>
+      <User/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import Appbar from './components/Appbar';
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <Appbar/>
+    </div>
+  );
+}
+
+export default Home;
